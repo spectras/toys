c24f75c0844d97d9f944b62fc976c69d83dbdf76
Julien Hartmann <julien@etherdream.org>
2018-06-04 15:25:23 +0000

    Cleanup gl::Buffer implementation

--- cubes/include/gl/Buffer.h
+++ cubes/include/gl/Buffer.h
@@ -22,6 +22,7 @@ enum class target {
     Uniform = GL_UNIFORM_BUFFER                         ///< Uniform data for shader use
 };
 
+/****************************************************************************/
 
 /** GPU Buffer
  *
@@ -29,68 +30,81 @@ enum class target {
  * Buffer types are incompatible, and each have a global binding point.
  * That is, as most one buffer of each type may be bound at any time, and binding
  * another buffer of the same type simply unbinds the previous one.
+ *
+ * When compiled in debug mode, all methods will assert this object really is
+ * the currently bound buffer for the relevant target. Those checks are removed
+ * if NDEBUG is defined.
  */
 template <target Target> class Buffer final
 {
-    typedef GLuint id_type;                 ///< Iternal type of buffer identifier
+    using id_type = GLuint;                     ///< Iternal type of buffer identifier
     static constexpr id_type invalid_id = 0;    ///< Sentinel value for invalid buffer object
-public:
 
+public:
     /// Hints to driver for optimisation
     enum class usage {
-        StaticDraw = GL_STATIC_DRAW,        ///< The data will be intialized once and read many times
-        DynamicDraw = GL_DYNAMIC_DRAW,      ///< The data will be updated every now and then
-        StreamDraw = GL_STREAM_DRAW,        ///< The data will be updated at every use
-        StaticRead = GL_STATIC_READ,
-        DynamicRead = GL_DYNAMIC_READ,
-        StreamRead = GL_STREAM_READ,
-        StaticCopy = GL_STATIC_COPY,
-        DynamicCopy = GL_DYNAMIC_COPY,
-        StreamCopy = GL_STREAM_COPY,
+        StaticDraw = GL_STATIC_DRAW,    ///< Data is intialized once and used to draw many times
+        DynamicDraw = GL_DYNAMIC_DRAW,  ///< Data is updated often and used to draw many times
+        StreamDraw = GL_STREAM_DRAW,    ///< Data is updated often and used to draw a few times
+        StaticRead = GL_STATIC_READ,    ///< Data is intialized once and read from app many times
+        DynamicRead = GL_DYNAMIC_READ,  ///< Data is updated often and read from app many times
+        StreamRead = GL_STREAM_READ,    ///< Data is updated often and read from app a few times
+        StaticCopy = GL_STATIC_COPY,    ///< Data is intialized once and copied to other buffer many times
+        DynamicCopy = GL_DYNAMIC_COPY,  ///< Data is updated often and used to read from GL many times
+        StreamCopy = GL_STREAM_COPY,    ///< Data is updated often and used to read from GL a few times
     };
 
 public:
-    Buffer()
-     : m_id(invalid_id), m_size(0)
+    Buffer() : m_id(invalid_id), m_size(0)
     {
         glGenBuffers(1, &m_id);
         if (m_id == invalid_id) { throw gl::error("glGenBuffers failed"); }
     }
 
+    // A buffer cannot be copied
     Buffer(const Buffer &) = delete;
-    Buffer(Buffer && other) noexcept
-        : m_id(invalid_id), m_size(0)
-        { std::swap(m_id, other.m_id); std::swap(m_size, other.m_size); }
-    Buffer &    operator=(const Buffer &) = delete;
+    Buffer & operator=(const Buffer & rhs) = delete;
 
-    Buffer & operator=(Buffer && other)
+    // A buffer supports move semantics
+    Buffer(Buffer && rhs) noexcept : m_id(invalid_id), m_size(0)
+        { swap(rhs); }
+    Buffer & operator=(Buffer && rhs)
     {
-        assert(this != &other);
-        if (m_id != invalid_id) { glDeleteBuffers(1, &m_id); }
-        m_id = other.m_id;
-        m_size = other.m_size;
-        other.m_id = invalid_id;
+        if (this != &rhs) {
+            clear();
+            swap(rhs);
+        }
         return *this;
     }
 
     ~Buffer()
+        { clear(); }
+
+    /// Destroy underlying OpenGL buffer, making this object invalid
+    void clear()
     {
-        if (m_id != invalid_id) { glDeleteBuffers(1, &m_id); }
+        if (m_id == invalid_id) { return; }
+        glDeleteBuffers(1, &m_id);
+        m_id = invalid_id;
     }
 
-    id_type     id() const noexcept { return m_id; }
+    /// Get OpenGL buffer identifier
+    id_type id() const noexcept
+        { return m_id; }
+
+    /// Get size of buffer in bytes
     std::size_t size() const noexcept
-        { return m_size; }                  ///< Buffer length, in bytes
+        { return m_size; }
 
     /// Discard data
-    inline void setData(std::nullptr_t)
+    void setData(std::nullptr_t)
     {
         assert(s_bound == m_id);
         glBufferData(static_cast<GLenum>(Target), m_size, nullptr, GL_DYNAMIC_DRAW);
     }
 
     /// Discard data and resize buffer
-    inline void setData(std::nullptr_t, std::size_t size)
+    void setData(std::nullptr_t, std::size_t size)
     {
         assert(s_bound == m_id);
         m_size = size;
@@ -98,7 +112,7 @@ public:
     }
 
     /// Initialize a new data area from a raw buffer
-    inline void setData(const void * ptr, std::size_t size, usage u)
+    void setData(const void * ptr, std::size_t size, usage u)
     {
         assert(s_bound == m_id);
         m_size = size;
@@ -106,13 +120,13 @@ public:
     }
 
     /// Initialize a new data area from a container type
-    template <typename T> inline void setData(const T & data, usage u)
+    template <typename T> void setData(const T & data, usage u)
     {
         setData(data.data(), data.size() * sizeof(typename T::value_type), u);
     }
 
     /// Update data from a raw buffer
-    inline void setData(const void * ptr, std::size_t size, std::size_t offset)
+    void setData(const void * ptr, std::size_t size, std::size_t offset)
     {
         assert(s_bound == m_id);
         assert(offset + size <= m_size);
@@ -120,27 +134,35 @@ public:
     }
 
     /// Update data from a container type
-    template <typename T> inline void setData(const T & data, std::size_t offset)
+    template <typename T> void setData(const T & data, std::size_t offset)
     {
         setData(data.data(), data.size() * sizeof(typename T::value_type), offset);
     }
 
     /// Bind buffer, enabling the use of other methods
-    inline void bind() const {
+    void bind() const {
         if (s_bound == m_id) { return; }
         glBindBuffer(static_cast<GLenum>(Target), m_id);
         s_bound = m_id;
     }
 
     /// Unbind buffer, leaving no active bound buffer
-    inline static void unbind() {
+    static void unbind() {
         if (s_bound == invalid_id) { return; }
         glBindBuffer(static_cast<GLenum>(Target), invalid_id);
         s_bound = invalid_id;
     }
 
     /// Get id of currently bound OpenGL buffer
-    inline static id_type bound() noexcept { return s_bound; }
+    static id_type bound() noexcept { return s_bound; }
+
+    /// Swap this buffer with another
+    void swap(Buffer & rhs) noexcept
+    {
+        using std::swap;
+        swap(m_id, rhs.m_id);
+        swap(m_size, rhs.m_size);
+    }
 private:
     id_type     m_id;           ///< OpenGL buffer identifier
     std::size_t m_size;         ///< Size of buffer in bytes
@@ -148,12 +170,18 @@ private:
     static id_type s_bound;     ///< Identifier of buffer that is currently bound
 };
 
+/// Swap two buffers, enabling ADL semantics
+template <target Target> void swap(Buffer<Target> & lhs, Buffer<Target> & rhs) noexcept
+{
+    lhs.swap(rhs);
+}
 
 template <target Target> constexpr typename Buffer<Target>::id_type Buffer<Target>::invalid_id;
 template <target Target> typename Buffer<Target>::id_type Buffer<Target>::s_bound = invalid_id;
 
 /****************************************************************************/
 
+// Convenient aliases for all targets
 typedef Buffer<target::Array> VertexBuffer;
 typedef Buffer<target::ElementArray> ElementBuffer;
 typedef Buffer<target::PixelPack> PixelPackBuffer;
